036e814ff821a659e4f8ee8068d1038fdb38040e
Dmytro Lutsenko <hone.domitoro@gmail.com>
2023-03-07 12:05:14 +0200

    test.js functions

--- eloquent_javascript/playground/test.js
+++ eloquent_javascript/playground/test.js
@@ -1,4 +1,4 @@
-function exampleCount () {
+function exampleCount () { // count a sum of numbers 1-10
     let total = 0, count = 1;
     while (count <= 10) {
         total += count;
@@ -6,4 +6,12 @@ function exampleCount () {
         console.log(total); // → 55
     }
 }
-exampleCount();
+
+function listCount(num) { // takes in a number and logs a sum of all numbers between 0 and that number (sum of 1-x)
+    let total = 0; count = 1;
+    while (count <= num) {
+        total += count;
+        count += 1;
+    }
+    console.log(total);
+}
