6101ba277a766065ccb9cacb3f6406d5aefb26b8
Dmytro Lutsenko <hone.domitoro@gmail.com>
2023-03-09 22:40:22 +0200

    php variable tests

--- php_playground/index.php
+++ php_playground/index.php
@@ -4,4 +4,10 @@ echo "She said \"hi\" to the dog.";
 $php_variable = "\nstring example\n";
 echo $php_variable;
 echo "string example two";
+$userChunky = "\nChuppy";
+$user_name =& $userChunky;
+
+$user_name .= ", huppy";
+$userChunky .= ", kappy";
+echo $user_name;
 ?>
