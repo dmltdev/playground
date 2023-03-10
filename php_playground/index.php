8d006d226d115f89e874efdf71f7cf0186dd7495
Dmytro Lutsenko <hone.domitoro@gmail.com>
2023-03-10 17:04:14 +0200

    save play

--- php_playground/index.php
+++ php_playground/index.php
@@ -1,13 +1,9 @@
 <?php
+function expectTwo($first, $second)
+{
+  return "whatever";
+}
+ 
+echo expectTwo("test", "why");
 
-echo "She said \"hi\" to the dog.";
-$php_variable = "\nstring example\n";
-echo $php_variable;
-echo "string example two";
-$userChunky = "\nChuppy";
-$user_name =& $userChunky;
-
-$user_name .= ", huppy";
-$userChunky .= ", kappy";
-echo $user_name;
 ?>
