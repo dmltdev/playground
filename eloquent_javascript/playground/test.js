1ddbc3a100c1836d050109b83410d09c9cd74542
Dmytro Lutsenko <hone.domitoro@gmail.com>
2023-03-13 10:41:04 +0200

    Math operators

--- eloquent_javascript/playground/test.js
+++ eloquent_javascript/playground/test.js
@@ -14,4 +14,9 @@ function listCount(num) { // takes in a number and logs a sum of all numbers bet
         count += 1;
     }
     console.log(total);
+
 }
+console.log(2e2);
+console.log(2e8);
+console.log(2**8);
+console.log(Math.pow(2,8));
