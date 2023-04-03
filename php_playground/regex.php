3f1fbbe11e4bd014adfadd4161e3f642bf55e871
Dmytro Lutsenko <hone.domitoro@gmail.com>
2023-04-03 09:56:35 +0300

    Regex Test

--- php_playground/regex.php
+++ php_playground/regex.php
@@ -1,5 +1,6 @@
 <?php
 $regex_exp = "/\(?\d\s?\d\d\.?\)?-?\d?\s?\d\d\.?\-?\d\s?\d\d\d\d?/"; // regex
+$regex_new = "/^[(]*([0-9]{3})[- .)]*[0-9]{3}[- .]*[0-9]{4}$/"; // Test for 10-digit North American telephone numbers.
 
 // $pattern matches these string
 $str1 = "718-555-3810";
@@ -8,7 +9,24 @@ $str3 = "1 212 555 3821";
 $str4 = "(917)5551298";
 $str5 = "212.555.8731";
 
-echo preg_match($regex_exp, $str1);
+function inputValid($str, $regex) {
+    if (!preg_match($regex, $str)) {
+        echo "Input {$str} does not compare to the defined regular expression!" . PHP_EOL;
+    } else {
+        echo "Input {$str} matches the defined regular expression!" . PHP_EOL;
+    }
+}
+
+inputValid($str1, $regex_exp);
+inputValid($str2, $regex_exp);
+inputValid($str3, $regex_exp);
+inputValid($str4, $regex_exp);
+inputValid($str5, $regex_exp);
+inputValid($str1, $regex_new);
+inputValid($str2, $regex_new);
+inputValid($str3, $regex_new); // The string should not match $regex_new because it is in 11-digit format
+inputValid($str4, $regex_new);
+inputValid($str5, $regex_new);
 
 // PHP Regex Cheatsheet/Tester: https://www.phpliveregex.com/
 ?>
