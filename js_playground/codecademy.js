d470955e7746bc2fe453d5fe9fd504677c674075
Dmytro Lutsenko <hone.domitoro@gmail.com>
2023-04-10 09:24:34 +0300

    js accessing obj properties bracket notation

--- js_playground/codecademy.js
+++ js_playground/codecademy.js
@@ -1,11 +1,27 @@
 let spaceship = {
-    homePlanet: 'Earth',
-    color: 'silver',
     'Fuel Type': 'Turbo Fuel',
-    numCrew: 5,
-    flightPath: ['Venus', 'Mars', 'Saturn']
+    'Active Duty': true,
+    homePlanet: 'Earth',
+    numCrew: 5
   };
+  spaceship['Active Duty'];   // Returns true
+  spaceship['Fuel Type'];   // Returns  'Turbo Fuel'
+  spaceship['numCrew'];   // Returns 5
+  spaceship['!!!!!!!!!!!!!!!'];   // Returns undefined
+
+  let returnAnyProp = (objectName, propName) => objectName[propName];
+ 
+returnAnyProp(spaceship, 'homePlanet'); // Returns 'Earth'
+
+let spaceship = {
+    'Fuel Type' : 'Turbo Fuel',
+    'Active Mission' : true,
+    homePlanet : 'Earth', 
+    numCrew: 5
+   };
+  
+  let propName =  'Active Mission';
   
   // Write your code below
-  const crewCount = spaceship.numCrew;
-  const planetArray = spaceship.flightPath;
+  const isActive = spaceship['Active Mission'];
+  console.log(spaceship[propName]);
