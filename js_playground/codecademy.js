92f1abbcde490a2c487c81853bac9cb01f378e24
Dmytro Lutsenko <hone.domitoro@gmail.com>
2023-04-10 09:39:21 +0300

    JS Nested Objects

--- js_playground/codecademy.js
+++ js_playground/codecademy.js
@@ -1,13 +1,43 @@
-let retreatMessage = 'We no longer wish to conquer your planet. It is full of dogs, which we do not care for.';
-
-// Write your code below
-let alienShip = {
-  retreat: function() {
-    console.log(retreatMessage);
-  },
-  takeOff: function() {
-    console.log('Spim... Borp... Glix... Blastoff!');
-  }
-}
-alienShip.retreat();
-alienShip.takeOff();
+let spaceship = {
+    passengers: null,
+    telescope: {
+      yearBuilt: 2018,
+      model: "91031-XLT",
+      focalLength: 2032 
+    },
+    crew: {
+      captain: { 
+        name: 'Sandra', 
+        degree: 'Computer Engineering', 
+        encourageTeam() { console.log('We got this!') },
+       'favorite foods': ['cookies', 'cakes', 'candy', 'spinach'] }
+    },
+    engine: {
+      model: "Nimbus2000"
+    },
+    nanoelectronics: {
+      computer: {
+        terabytes: 100,
+        monitors: "HD"
+      },
+      'back-up': {
+        battery: "Lithium",
+        terabytes: 50
+      }
+    }
+  }; 
+  
+  let capFave = spaceship.crew.captain['favorite foods'][0];
+  spaceship.passengers = [
+    {
+      name: 'Michael Jackson',
+      birthdate: '1992-05-22'
+    },
+    {
+      name: 'Kate Lenon',
+      birthdate: '1985-11-07'
+    }
+  ]
+  
+  let firstPassenger = spaceship.passengers[0];
+  console.log(firstPassenger);
