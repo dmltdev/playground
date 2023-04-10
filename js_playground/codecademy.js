24195bdaba49713f968e05a54a94114412c10d73
Dmytro Lutsenko <hone.domitoro@gmail.com>
2023-04-10 09:21:27 +0300

    js accessing properties

--- js_playground/codecademy.js
+++ js_playground/codecademy.js
@@ -1,5 +1,11 @@
-// Write your fasterShip object literal below
-let fasterShip = {
+let spaceship = {
+    homePlanet: 'Earth',
     color: 'silver',
-    'Fuel Type': 'Turbo Fuel'
-  }
+    'Fuel Type': 'Turbo Fuel',
+    numCrew: 5,
+    flightPath: ['Venus', 'Mars', 'Saturn']
+  };
+  
+  // Write your code below
+  const crewCount = spaceship.numCrew;
+  const planetArray = spaceship.flightPath;
