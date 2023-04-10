a1b16c988d7b9371e20b4788012d48f7f962cb06
Dmytro Lutsenko <hone.domitoro@gmail.com>
2023-04-10 09:26:53 +0300

    js obj property assignment

--- js_playground/codecademy.js
+++ js_playground/codecademy.js
@@ -1,27 +1,11 @@
 let spaceship = {
-    'Fuel Type': 'Turbo Fuel',
-    'Active Duty': true,
-    homePlanet: 'Earth',
-    numCrew: 5
-  };
-  spaceship['Active Duty'];   // Returns true
-  spaceship['Fuel Type'];   // Returns  'Turbo Fuel'
-  spaceship['numCrew'];   // Returns 5
-  spaceship['!!!!!!!!!!!!!!!'];   // Returns undefined
-
-  let returnAnyProp = (objectName, propName) => objectName[propName];
- 
-returnAnyProp(spaceship, 'homePlanet'); // Returns 'Earth'
-
-let spaceship = {
     'Fuel Type' : 'Turbo Fuel',
-    'Active Mission' : true,
-    homePlanet : 'Earth', 
-    numCrew: 5
-   };
-  
-  let propName =  'Active Mission';
+    homePlanet : 'Earth',
+    color: 'silver',
+    'Secret Mission' : 'Discover life outside of Earth.'
+  };
   
   // Write your code below
-  const isActive = spaceship['Active Mission'];
-  console.log(spaceship[propName]);
+  spaceship.color = 'glorious gold';
+  spaceship.numEngines = 5;
+  delete spaceship['Secret Mission'];
