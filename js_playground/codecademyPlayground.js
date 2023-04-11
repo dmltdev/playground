5580cf8f6242e1b04019e329cc49e233e5f2032e
Dmytro Lutsenko <hone.domitoro@gmail.com>
2023-04-11 17:09:00 +0300

    completing team stats challenge codecademy js

---
+++ js_playground/codecademyPlayground.js
@@ -0,0 +1,9 @@
+const menu = {
+    _meal: '',
+    _price: 0,
+    set meal(mealToCheck) {
+        if (typeof mealToCheck === 'string') {
+            this._meal = mealToCheck;
+        }
+    }
+  }
