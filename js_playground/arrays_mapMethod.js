b907330e96ee0543a4bbf07c80f40d03fd38efb3
Domitoro <hone.domitoro@gmail.com>
2023-04-13 23:11:13 +0300

    array map object map js

---
+++ js_playground/arrays_mapMethod.js
@@ -0,0 +1,18 @@
+// array.map() is a method that takes a function as an argument 
+// and returns a new array with the results of calling the function on each element in the original array.
+
+// Example
+const numbers = [1, 2, 3, 4, 5];
+const doubled = numbers.map(number => number * 2);
+console.log(doubled); // [2, 4, 6, 8, 10]
+
+// Example
+const words = ['asap', 'byob', 'rsvp', 'diy'];
+const acronym = words.map(word => word.toUpperCase().split('').join('.'));
+console.log(acronym); // ['A.S.A.P', 'B.Y.O.B', 'R.S.V.P', 'D.I.Y']
+
+// Example
+const prices = [6.75, 3.10, 4.00, 8.12];
+const tax = 0.07;
+const taxAdjustedPrices = prices.map(price => price * (1 + tax));
+console.log(taxAdjustedPrices); // [7.2025, 3.347, 4.28, 8.7244]
--- js_playground/builtInObjects_objMethods.js
+++ js_playground/builtInObjects_objMethods.js
@@ -35,3 +35,23 @@ console.log(robotProperty); // true
 // Object.valueOf() returns the primitive value of the specified object.
 const robotValue = robot.valueOf();
 console.log(robotValue); // { model: '1E78V2', energyLevel: 100, functionality: { beep: [Function: beep], fireLaser: [Function: fireLaser] } }
+
+// Object.map() is a method that takes a function as an argument 
+// and returns a new object with the results of calling the function on each element in the original object.
+const products = [
+    { name: 'gold star', price: 20 },
+    { name: 'mushroom', price: 40 },
+    { name: 'green shells', price: 30 },
+    { name: 'banana skin', price: 10 },
+    { name: 'red shells', price: 50 }
+];
+
+const saleProducts = products.map(product => {
+    if (product.price > 30) {
+        return { name: product.name, price: product.price / 2 }
+    } else {
+        return product;
+    }
+});
+
+console.log(saleProducts, products);
