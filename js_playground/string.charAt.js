15c496ff9b6f2ccf9ea5ccc57e0dd6b323c2290c
Matt <hone.domitoro@gmail.com>
2023-04-18 08:46:22 +0300

    string char at js

---
+++ js_playground/string.charAt.js
@@ -0,0 +1,16 @@
+// string.charAt() method returns the character at the specified index in a string.
+
+// The index of the first character is 0, the second character is 1, and so on.
+
+// If the index you specify is greater than or equal to the length of the string, JavaScript returns an empty string.
+
+// If the index you specify is a negative number, the position is counted from the end of the string.
+
+// Example
+
+const str = 'Hello World!';
+console.log(str.charAt(0)); // H
+console.log(str.charAt(1)); // e
+console.log(str.charAt(2)); // l
+console.log(str.charAt(3)); // l
+console.log(str.charAt(4)); // o
