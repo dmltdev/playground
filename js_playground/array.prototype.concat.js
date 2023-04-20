4da1514e86cd5368f5035aeae421468cf038e0a7
Dmytro Lutsenko <hone.domitoro@gmail.com>
2023-04-20 22:47:22 +0300

    js array contact

---
+++ js_playground/array.prototype.concat.js
@@ -0,0 +1,9 @@
+// array.prototype.concat() method is used to merge two or more arrays. This method does not change the existing arrays, but instead returns a new array.
+
+const array1 = ['a', 'b', 'c'];
+
+const array2 = ['d', 'e', 'f'];
+
+const array3 = array1.concat(array2);
+
+console.log(array3);
