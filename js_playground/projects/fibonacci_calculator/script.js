ac6612ebed7d67b848a25aa816d337f723044265
Dmytro Lutsenko <hone.domitoro@gmail.com>
2023-04-30 22:29:32 +0300

    fib calc v2

--- js_playground/projects/fibonacci_calculator/script.js
+++ js_playground/projects/fibonacci_calculator/script.js
@@ -25,5 +25,46 @@ function autoHide() {
 
 // Clear answer div and input values
 const clear = () => {
-    document.getElementById('fibonacci').innerHTML = '';
+    let fibSelect = document.getElementById('fib-select');
+    let fibOption = fibSelect.selectedOptions[0].value;
+    let amswerBtn = document.getElementById('answer');
+    answerBtn.value = '';
+    if (fibOption === 'number-option') {
+        document.getElementById('fib-num').value = '';
+    } else {
+        document.getElementById('n1').value = '';
+        document.getElementById('n2').value = '';
+    }
+}
+
+// Calculate fibonacci sequence
+
+const fibCalculate = () => {
+    let answerBtn = document.getElementById('answer');
+    answerBtn.value = '';
+    if (fibOption === 'number-option') {
+        let num = document.getElementById('fib-num').value;
+        let fibA = 0;
+        let fibB = 1;
+        for (let i = 2; i <= num; i++) {
+            let fibNum = fibA + fibB; 
+            fibA = fibB;
+            fibB = fibNum;
+        }
+        answerBtn.value = fibB;
+    } else if (fibOption === 'sequence-option') {
+        let num1 = document.getElementById('n1').value;
+        let num2 = document.getElementById('n2').value;
+        let numDiff = Math.abs(num1 - num2); 
+        let fibA = 0; 
+        let fibB = 1; 
+        for (let i = 2; i <= numDiff + 1; i++) {
+            let fibNum = fibA + fibB; 
+            fibA = fibB;
+            fibB = fibNum;
+        }
+        answerBtn.value = fibB + fibA;
+    } else {
+        return "Invalid option chosen. Choose Number of Sequence.";
+    }
 }
