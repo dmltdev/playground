e3ca9edab48f136f4fe987f5ae8501f00b8d55b6
Dmytro Lutsenko <hone.domitoro@gmail.com>
2023-05-20 11:32:48 +0300

    header, color palette adjustments

---
+++ playgrounds/js_playground/codewars/likesVSdislikes.js
@@ -0,0 +1,10 @@
+// Like, Dislike, Nothing come from Preloaded
+
+function likeOrDislike(buttons) {
+    let active = [];
+    if (buttons.length === 1) {
+      return buttons;
+    }
+  }
+
+  console.log(likeOrDislike(['Dislike']));
