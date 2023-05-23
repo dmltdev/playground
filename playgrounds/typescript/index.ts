44606de63cb349c61f28b7249067ea121c37ce98
Dmytro Lutsenko <hone.domitoro@gmail.com>
2023-05-23 09:23:37 +0300

    ts 2

--- playgrounds/typescript/index.ts
+++ playgrounds/typescript/index.ts
@@ -10,3 +10,4 @@ const Person : {firstName?: string, lastName?: string, birthdate?: object, hobbi
 };
 
 console.log(Person);
+// TODO Fix the issue with the implicit conversion of the "Person" variable type from "const" to "var"
