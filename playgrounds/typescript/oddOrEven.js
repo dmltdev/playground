f37e9d2ade3b21b27ce52df270cc464ff779c71a
Dmytro Lutsenko <hone.domitoro@gmail.com>
2023-05-29 08:42:09 +0300

    ts practice codewars may29`23

---
+++ playgrounds/typescript/oddOrEven.js
@@ -0,0 +1,3 @@
+const oddOrEven = (array) => array.reduce((a, i) => a + i, 0) % 2 === 0 ? "even" : "odd";
+console.log(oddOrEven([1, 2, 5]));
+//# sourceMappingURL=oddOrEven.js.map
---
+++ playgrounds/typescript/oddOrEven.ts
@@ -0,0 +1,3 @@
+const oddOrEven = (array:number[]):string => array.reduce((a,i) => a + i, 0) % 2 === 0 ? "even" : "odd";
+
+console.log(oddOrEven([1, 2, 5]));
