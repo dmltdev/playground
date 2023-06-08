17202048d2c38ad0d4a3034dbd43f2b96e37b625
Dmytro Lutsenko <hone.domitoro@gmail.com>
2023-06-08 10:35:20 +0300

    eod 20230608

--- playgrounds/typescript/codewars/testing123.ts
+++ playgrounds/typescript/codewars/testing123.ts
@@ -1,3 +1,18 @@
+/*
+//!First method using for loop
 export function number(array: string[]): string[]{
-    return []
+    for (let i:number = 0; i < array.length; i++) {
+        let word:string = array[i];
+        array[i] = `${i+1}: ${word}`;
+    }
+    return array;
+};
+*/
+
+//! Second method using array.map
+export function number(array: string[]): string[]{
+    return array.map((el, i) => `${i+1}: ${el}`);
   }
+
+let arr:string[] = ["a", "b", "c", "d"];
+console.log(number(arr));
