56988e99026ea3d6d5814cac6174b45ca3793e60
Domitoro <hone.domitoro@gmail.com>
2023-06-11 14:03:23 +0300

    leetcode array101 challenges

---
+++ playgrounds/js_playground/leetcode/arrays101/squaresOfASortedArray.js
@@ -0,0 +1,13 @@
+// Squares of a Sorted Array
+// Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+const sortedSquares = function(nums) {
+    nums.forEach((num,index) => {
+        nums[index] = Math.pow(num, 2);
+    })
+    nums.sort((a,b) => a - b);
+    return nums;
+};
+
+let nums1 = [-4,-1,0,3,10]
+console.log(sortedSquares(nums1));
