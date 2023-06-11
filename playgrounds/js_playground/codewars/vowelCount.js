4162d0e10a5988b021a79cf5f1e4aa9f5d5f660b
Domitoro <hone.domitoro@gmail.com>
2023-06-11 14:21:43 +0300

    removecomments in tsc set to true

--- playgrounds/js_playground/codewars/vowelCount.js
+++ playgrounds/js_playground/codewars/vowelCount.js
@@ -1,3 +1,13 @@
+//! Method 1: use array.filter and array of vowels
+/*
+export class Kata {
+  static getCount(str: string): number {
+    let vowels: string[] = ['a','e','i','o','u'];
+    return str.split('').filter(letter => vowels.includes(letter)).length;
+  }
+}
+*/
+//! Method 2: use regex str.prototype.match
 export class Kata {
     static getCount(str) {
         let filter = str.match(/[aeiou]/gi);
--- playgrounds/js_playground/leetcode/counter2.js
+++ playgrounds/js_playground/leetcode/counter2.js
@@ -7,8 +7,8 @@ function createCounter(init) {
     };
 }
 ;
-let counter = createCounter(5);
-counter.increment();
-counter.reset();
-counter.decrement();
+let counter = createCounter(5); //?
+counter.increment(); //?
+counter.reset(); //?
+counter.decrement(); //?
 //# sourceMappingURL=counter2.js.map
