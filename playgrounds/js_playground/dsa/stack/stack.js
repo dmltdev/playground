84eba34a72e2829a02f9f7c7a8672aa0f64e54e1
Dmytro Lutsenko <hone.domitoro@gmail.com>
2023-06-15 09:59:06 +0300

    using stack for postfix evaliation

--- playgrounds/js_playground/dsa/stack/stack.js
+++ playgrounds/js_playground/dsa/stack/stack.js
@@ -56,3 +56,46 @@ stack.push(4);
 stack.push(5);
 
 console.log(stack.printStack());
+
+// Performs Postfix Evaluation on a given exp
+function postFixEvaluation(exp)
+{
+    let stack = new Stack();
+    for (let i = 0; i < exp.length; i++) {
+        let c = exp[i];
+        if (!isNaN(c))
+            stack.push(c - '0');
+        else {
+            let val1 = stack.pop();
+            let val2 = stack.pop();
+            if (val1 == "Underflow" || val2 == "Underflow")
+                return "Can't perform postfix evaluation";
+            switch (c) {
+            case '+':
+                stack.push(val2 + val1);
+                break;
+ 
+            case '-':
+                stack.push(val2 - val1);
+                break;
+ 
+            case '/':
+                stack.push(val2 / val1);
+                break;
+ 
+            case '*':
+                stack.push(val2 * val1);
+                break;
+            }
+        }
+    }
+ 
+    return stack.pop();
+}
+ 
+// calling the above method
+// returns 9
+console.log(postFixEvaluation("235*+8-"));
+ 
+// returns postfix evaluation can't be performed
+// console.log(postFixEvaluation("23*+"));
