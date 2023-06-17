afca5769c5823d9c7091c8d6ff9d4c68f91f8cee
Dmytro Lutsenko <hone.domitoro@gmail.com>
2023-06-17 08:11:58 +0300

    codewars 7kyu twoOldestAges solved

--- playgrounds/js_playground/codewars/7kyu.twoOldestAges.js
+++ playgrounds/js_playground/codewars/7kyu.twoOldestAges.js
@@ -7,3 +7,11 @@ The two oldest ages function/method needs to be completed. It should take an arr
 
 The order of the numbers passed in could be any order. The array will always include at least 2 items. If there are two or more oldest age, then return both of them in array format.
 */
+
+//! It would be better to use .toSpliced() not to mutate original array. However, VSCode currently does not recognize this ES2023 array method.
+function twoOldestAges(ages){
+    return ages.sort((a,b) => a - b).splice(-2);
+}
+
+let arr1 = [6,5,83,5,3,18];
+console.log(twoOldestAges(arr1));
