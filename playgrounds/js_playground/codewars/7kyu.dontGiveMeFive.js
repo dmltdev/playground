77501b5d89152f5a8f68b01eec3387d12574018b
Dmytro Lutsenko <hone.domitoro@gmail.com>
2023-06-18 15:07:21 +0300

    7kyu dont give me five solved

---
+++ playgrounds/js_playground/codewars/7kyu.dontGiveMeFive.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/5813d19765d81c592200001a
+
+function dontGiveMeFive(start, end) {
+    let res = 0;
+    for (let i = start; i <= end; i++) {
+        if (!Math.abs(i).toString().includes('5')) {
+            res++;
+        }
+    }
+    return res;
+}
+
+console.log(dontGiveMeFive(4,26));
