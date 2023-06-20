e1a35fa03da564e83d012f50014ce1c1936f2d77
Dmytro Lutsenko <hone.domitoro@gmail.com>
2023-06-20 12:04:36 +0300

    js queue practice

---
+++ playgrounds/js_playground/javascript/Queue.js
@@ -0,0 +1,55 @@
+class Queue {
+    constructor() {
+      this.elements = {};
+      this.head = 0;
+      this.tail = 0;
+    }
+    enqueue(element) {
+      this.elements[this.tail] = element;
+      this.tail++;
+    }
+    dequeue() {
+      const item = this.elements[this.head];
+      delete this.elements[this.head];
+      this.head++;
+      return item;
+    }
+    peek() {
+      return this.elements[this.head];
+    }
+    get length() {
+      return this.tail - this.head;
+    }
+    get isEmpty() {
+      return this.length === 0;
+    }
+  }
+  
+  //! Example
+  let q = new Queue();
+  for (let i = 1; i <= 7; i++) {
+    q.enqueue(i);
+  }
+  // get the current item at the front of the queue
+  console.log(q.peek()); // 1
+  
+  // get the current length of queue
+  console.log(q.length); // 7
+  
+  // dequeue all elements
+  while (!q.isEmpty) {
+    console.log(q.dequeue());
+  }
+
+  //! Testing
+  let quota = new Queue();
+  q.enqueue(5);
+  q.enqueue({Master: "Kim", Kyu: 8})
+  q.enqueue([3, 4, 6])
+  console.log(q.peek());
+  q.dequeue();
+  console.log(q.peek());
+  q.dequeue();
+  console.log(q.peek());
+  q.dequeue();
+  console.log(q.peek());
