3e746c511226134b333b3aac5fd6e868f96aa685
Dmytro Lutsenko <hone.domitoro@gmail.com>
2023-06-23 07:53:08 +0300

    leetcode easy 2666 solved

--- playgrounds/js_playground/javascript/test.js
+++ playgrounds/js_playground/javascript/test.js
@@ -1,5 +1,10 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-console.log(arr.length);
-
-// arr.length is a property of the array object that returns the number of elements in the array. 
-// In this case, the array has 10 elements, so arr.length returns 10.
+function withLogging(fn) {
+    return function(...args) {
+      console.log('Arguments', args);
+      const result = fn(...args);
+      console.log('Result', result);
+      return result;
+    }
+  }
+  const add = withLogging((a, b) => a + b);
+  add(1, 2);
