16551adde011c0dbb0319ae48999f83e2c666686
Dmytro Lutsenko <hone.domitoro@gmail.com>
2023-06-24 08:22:33 +0300

    leetcode easy 2715 solved

---
+++ playgrounds/js_playground/codewars/7kyu.findMissingElementBetweenTwoArrays.js
@@ -0,0 +1,12 @@
+function findMissing(arr1, arr2) {
+    let arr = [];
+    for (let i = 0; i < arr1.length; i++) {
+        
+    }
+    return arr;
+}
+
+let array1 = [1, 2, 3];
+let array2 = [1, 3];
+
+console.log(findMissing(array1, array2));
