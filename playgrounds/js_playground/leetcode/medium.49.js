afa700285c806f86cd0614aa1cf7aea77674083a
Dmytro Lutsenko <hone.domitoro@gmail.com>
2023-06-29 06:55:59 +0300

    leetcode medium 49 solved

---
+++ playgrounds/js_playground/leetcode/medium.49.js
@@ -0,0 +1,12 @@
+const groupAnagrams = function(strs) {
+    let map = new Map();
+    for (let str of strs) {
+        const letter = str.split('').sort().join('')
+        let result = map.get(letter) || []
+        result.push(str);
+        map.set(letter, result)
+    }
+    return Array.from(map.values())
+};
+
+console.log(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
