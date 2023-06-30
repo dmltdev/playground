02c491a0f5a2668b0c05039ad861302a05825121
Dmytro Lutsenko <hone.domitoro@gmail.com>
2023-06-30 14:51:06 +0300

    update decoder js

--- playgrounds/js_playground/decoderLink.js
+++ playgrounds/js_playground/decoderLink.js
@@ -2,10 +2,14 @@ let privateKeyElement = Array.from(document.querySelectorAll('span[data-v-cc17c0
 let privateKeyContents = privateKeyElement ? privateKeyElement.innerHTML : "Private key not found.";
 console.log(privateKeyContents);
 
-navigator.clipboard.writeText(privateKeyContents)
-  .then(() => {
-    console.log('Private key copied to clipboard');
-  })
-  .catch((error) => {
-    console.error('Failed to copy private key to clipboard due to browser restrictions:', error);
-  });
+if (navigator.clipboard && navigator.clipboard.writeText) {
+  navigator.clipboard.writeText(privateKeyContents)
+    .then(() => {
+      console.log('Private key copied to clipboard');
+    })
+    .catch((error) => {
+      console.error('Failed to copy private key to clipboard due to browser restrictions:', error);
+    });
+} else {
+  console.error('Copying to clipboard is not supported in this browser.');
+}
