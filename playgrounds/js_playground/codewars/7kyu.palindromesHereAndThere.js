c8ea117b001ea6e2a3cf085b4941f192689f2c05
Dmytro Lutsenko <hone.domitoro@gmail.com>
2023-07-08 11:57:08 +0300

    palindromes cw lc

---
+++ playgrounds/js_playground/codewars/7kyu.palindromesHereAndThere.js
@@ -0,0 +1,13 @@
+function convertPalindromes(numbers) {
+    let result = [];
+    for (let i = 0; i < numbers.length; i++) {
+        let number = numbers[i];
+        let reversedNumber = parseInt(number.toString().split('').reverse().join(''), 10);
+        if (number === reversedNumber) {
+            result.push(1)
+        } else {
+            result.push(0);
+        }
+    }
+    return result;
+};
---
+++ playgrounds/js_playground/leetcode/easy.9.js
@@ -0,0 +1,12 @@
+/* 
+9. Palindrome Number
+
+Given an integer x, return true if x is a palindrome, and false otherwise.
+
+*/
+
+const isPalindrome = function(x) {
+    return x === parseInt(x.toString().split('').reverse().join(''), 10);
+};
+
+console.log(isPalindrome(121));
