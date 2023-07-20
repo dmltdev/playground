9d7ccd3614603e557a56904c7b4616a5bfbb63f8
Dmytro Lutsenko <hone.domitoro@gmail.com>
2023-07-20 08:30:59 +0300

    easy 67 solved

---
+++ projects/react_tic-tac-toe/src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Board from "./components/Board";
+
+export default function App() {
+	return (
+		<>
+			<Board />
+		</>
+	);
+}
---
+++ projects/react_tic-tac-toe/src/components/Board.jsx
@@ -0,0 +1,42 @@
+import PropTypes from "prop-types";
+
+Square.propTypes = {
+	value: PropTypes.string.isRequired,
+};
+
+function Square({ value }) {
+	function handleClick() {
+		console.log(`clicked ${value}!`);
+	}
+
+	return (
+    <button 
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+	return (
+		<>
+			<div className="board-row">
+				<Square value="1" />
+				<Square value="2" />
+				<Square value="3" />
+			</div>
+			<div className="board-row">
+				<Square value="4" />
+				<Square value="5" />
+				<Square value="6" />
+			</div>
+			<div className="board-row">
+				<Square value="7" />
+				<Square value="8" />
+				<Square value="9" />
+			</div>
+		</>
+	);
+}
