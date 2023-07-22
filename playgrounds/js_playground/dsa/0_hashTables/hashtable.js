9fc434d25a4e7b6d21bed7336a54d563e2c01f1e
Dmytro Lutsenko <hone.domitoro@gmail.com>
2023-07-22 13:20:39 +0300

    hashtable constructor

---
+++ playgrounds/js_playground/dsa/0_hashTables/hashtable.js
@@ -0,0 +1,7 @@
+export default class hashTable {
+    constructor() {
+        this.table = new Array(127);
+        this.size = 0;
+    }
+    
+}
--- projects/react_tic-tac-toe/src/App.jsx
+++ projects/react_tic-tac-toe/src/App.jsx
@@ -1,11 +1,11 @@
 import React, { Component, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import Game from "./components/Board";
+import Welcome from "./components/Welcome"
 
 export default function App() {
 	return (
 		<>
+			<Welcome />
 			<Game />
 		</>
 	);
